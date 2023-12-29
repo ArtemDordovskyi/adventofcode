30e623af46ea02ac27c89efd748c14692e4c01f6
Artem Dordovskyi <artemdordovskyi@outlook.com>
2023-12-30 01:10:14 +0200

    Pipe Maze. Slow. Part 2 / Day 10

--- day_10_pipe_maze/src/main.rs
+++ day_10_pipe_maze/src/main.rs
@@ -86,30 +86,59 @@ impl Point {
         points
     }
 
-    fn start(&self, points: HashMap<Point, char>) -> usize {
-        let mut ways = self.ways(points.clone());
-
-        let mut result = 1;
-        let mut previous_points = vec![self.clone(), self.clone()];
-        while !ways.iter().all(|e| e == &ways[0]) {
-            result += 1;
-            let mut new_ways = Vec::new();
-            for way in ways {
-                let prev = previous_points.remove(0);
-                let char = points[&way];
-                let coords: Point = way.next_point(prev, char).unwrap();
-                new_ways.push(coords.clone());
-                previous_points.push(way.clone());
-            }
-            ways = new_ways;
+    fn distance(&self, points: &HashMap<Point, char>) -> usize {
+        let poly = self.poly(points);
+        if poly.len() % 2 == 0 {
+            poly.len() / 2
+        } else {
+            (poly.len() + 1) / 2
         }
-        result
+    }
+
+    fn poly(&self, points: &HashMap<Point, char>) -> Vec<Point> {
+        let mut way = self.ways(points.clone())[0].clone();
+        let mut poly = Vec::new();
+        poly.push(way.clone());
+        let mut prev = self.clone();
+
+        while &way != self {
+            let char = points[&way];
+            let next_point = way.next_point(prev.clone(), char).unwrap();
+            prev = way.clone();
+            way = next_point;
+            poly.push(way.clone());
+        }
+
+        poly
+    }
+
+    //Ray casting algorithm
+    fn is_point_in_path(&self, poly: &[Point]) -> bool {
+        let mut j = poly.len() - 1;
+        let mut c = false;
+        for i in 0..poly.len() {
+            if (poly[i].0 == self.0) && (poly[i].1 == self.1) {
+                // point is a corner
+                return true;
+            }
+            if (poly[i].1 > self.1) != (poly[j].1 > self.1) {
+                let slope = (self.0 - poly[i].0) * (poly[j].1 - poly[i].1)
+                    - (poly[j].0 - poly[i].0) * (self.1 - poly[i].1);
+                if slope == 0 {
+                    // point is on boundary
+                    return true;
+                }
+                if (slope < 0) != (poly[j].1 < poly[i].1) {
+                    c = !c;
+                }
+            }
+            j = i;
+        }
+        c
     }
 }
 
-fn main() {
-    let time = std::time::Instant::now();
-    let input = std::fs::read_to_string("input.txt").unwrap();
+fn parse(input: String) -> HashMap<Point, char> {
     let mut points: HashMap<Point, char> = HashMap::new();
 
     for (i, line) in input.lines().enumerate() {
@@ -119,30 +148,63 @@ fn main() {
         }
     }
 
+    points
+}
+
+fn main() {
+    let time = std::time::Instant::now();
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let points = parse(input);
+
     let (start_point, _) = points.clone().into_iter().find(|(_, c)| c == &'S').unwrap();
-    let result = start_point.start(points);
-    println!("Result {:?} ({:?})", result, time.elapsed())
+    let result = start_point.distance(&points);
+    println!("Result {:?} ({:?})", result, time.elapsed());
+
+    let poly = start_point.poly(&points);
+    let checked: Vec<Point> = points
+        .clone()
+        .into_iter()
+        .filter(|(p, _)| !poly.contains(p) )
+        .map(|(p, _)| p)
+        .filter(|point| point.is_point_in_path(&poly))
+        .map(|p| p)
+        .collect();
+
+    println!("Result {:?} ({:?})", checked.len(), time.elapsed());
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-    use crate::Point;
+    use crate::{parse, Point};
 
     #[test]
     fn part_1() {
         let test = std::fs::read_to_string("test.txt").unwrap();
-        let mut points: HashMap<Point, char> = HashMap::new();
-
-        for (i, line) in test.lines().enumerate() {
-            let chars = line.chars();
-            for (j, char) in chars.enumerate() {
-                points.insert(Point(j as i32, i as i32), char);
-            }
-        }
+        let points = parse(test);
 
         let (start_point, _) = points.clone().into_iter().find(|(_, c)| c == &'S').unwrap();
-        let result = start_point.start(points);
+        let result = start_point.distance(&points);
+
         assert_eq!(result, 8)
+    }
+
+    #[test]
+    fn part_2() {
+        let test = std::fs::read_to_string("test2.txt").unwrap();
+        let points = parse(test);
+
+        let (start_point, _) = points.clone().into_iter().find(|(_, c)| c == &'S').unwrap();
+
+        let poly = start_point.poly(&points);
+        let checked: Vec<Point> = points
+            .clone()
+            .into_iter()
+            .filter(|(p, _)| !poly.contains(p) )
+            .map(|(p, _)| p)
+            .filter(|point| point.is_point_in_path(&poly))
+            .map(|p| p)
+            .collect();
+        let result = checked.len();
+        assert_eq!(result, 10)
     }
 }
