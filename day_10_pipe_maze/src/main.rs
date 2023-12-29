fbe563dd9f7e6cb131c448bc9e7f94221ee8acaa
Artem Dordovskyi <artemdordovskyi@outlook.com>
2023-12-30 00:07:21 +0200

    Optimization.

--- day_10_pipe_maze/src/main.rs
+++ day_10_pipe_maze/src/main.rs
@@ -1,104 +1,105 @@
-#[derive(Clone, Debug)]
-struct Point {
-    char: char,
-    coords: (i32, i32),
-}
+use std::collections::HashMap;
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct Point(i32, i32);
 
 impl Point {
-    fn next_point(&self, prev: Point) -> Option<(i32, i32)> {
-        let coords = match self.char {
+    fn next_point(&self, prev: Point, char: char) -> Option<Point> {
+        let coords = match char {
             '|' => {
-                if self.coords.1 - prev.coords.1 > 0 {
-                    (self.coords.0, self.coords.1 + 1)
+                if self.1 - prev.1 > 0 {
+                    (self.0, self.1 + 1)
                 } else {
-                    (self.coords.0, self.coords.1 - 1)
+                    (self.0, self.1 - 1)
                 }
             },
             '-' => {
-                if self.coords.0 - prev.coords.0 > 0 {
-                    (self.coords.0 + 1, self.coords.1)
+                if self.0 - prev.0 > 0 {
+                    (self.0 + 1, self.1)
 
                 } else {
-                    (self.coords.0 - 1, self.coords.1)
+                    (self.0 - 1, self.1)
                 }
             },
             'L' => {
-                if self.coords.1 - prev.coords.1 > 0 {
-                    (self.coords.0 + 1, self.coords.1)
+                if self.1 - prev.1 > 0 {
+                    (self.0 + 1, self.1)
 
                 } else {
-                    (self.coords.0, self.coords.1 - 1)
+                    (self.0, self.1 - 1)
                 }
             },
             'J' => {
-                if self.coords.1 - prev.coords.1 > 0 {
-                    (self.coords.0 - 1, self.coords.1)
+                if self.1 - prev.1 > 0 {
+                    (self.0 - 1, self.1)
 
                 } else {
-                    (self.coords.0, self.coords.1 - 1)
+                    (self.0, self.1 - 1)
                 }
             },
             '7' => {
-                if self.coords.0 - prev.coords.0 > 0 {
-                    (self.coords.0, self.coords.1 + 1)
+                if self.0 - prev.0 > 0 {
+                    (self.0, self.1 + 1)
 
                 } else {
-                    (self.coords.0 - 1, self.coords.1)
+                    (self.0 - 1, self.1)
                 }
             },
             'F' => {
-                if self.coords.0 - prev.coords.0 < 0 {
-                    (self.coords.0, self.coords.1 + 1)
+                if self.0 - prev.0 < 0 {
+                    (self.0, self.1 + 1)
 
                 } else {
-                    (self.coords.0 + 1, self.coords.1)
+                    (self.0 + 1, self.1)
                 }
             },
             _ => return None
         };
 
-        Some(coords)
+        Some(Point(coords.0, coords.1))
     }
 
-    fn ways(&self, points: Vec<Point>) -> Vec<Point> {
+    fn ways(&self, points: HashMap<Point, char>) -> Vec<Point> {
         let mut coords = Vec::new();
-        coords.push((self.coords.0 - 1, self.coords.1));
-        coords.push((self.coords.0 + 1, self.coords.1));
-        coords.push((self.coords.0, self.coords.1 + 1));
-        coords.push((self.coords.0, self.coords.1 - 1));
+        coords.push(Point(self.0 - 1, self.1));
+        coords.push(Point(self.0 + 1, self.1));
+        coords.push(Point(self.0, self.1 + 1));
+        coords.push(Point(self.0, self.1 - 1));
 
         let points: Vec<Point> = points
             .into_iter()
-            .filter(|p| coords.contains(&p.coords))
-            .filter(|p|
-                match p.char {
-                    '|' => p.coords.1 != self.coords.1,
-                    '-' => p.coords.0 != self.coords.0,
-                    'L' => (p.coords.1 - self.coords.1) == 1 || (p.coords.0 - self.coords.0) == -1,
-                    'J' => (p.coords.1 - self.coords.1) == 1 || (p.coords.0 - self.coords.0) == 1,
-                    '7' => (p.coords.1 - self.coords.1) == -1 || (p.coords.0 - self.coords.0) == 1,
-                    'F' => (p.coords.1 - self.coords.1) == -1 || (p.coords.0 - self.coords.0) == -1,
+            .filter(|(p, _)| coords.contains(p))
+            .filter(|(p, char)|
+                match char {
+                    '|' => p.1 != self.1,
+                    '-' => p.0 != self.0,
+                    'L' => (p.1 - self.1) == 1 || (p.0 - self.0) == -1,
+                    'J' => (p.1 - self.1) == 1 || (p.0 - self.0) == 1,
+                    '7' => (p.1 - self.1) == -1 || (p.0 - self.0) == 1,
+                    'F' => (p.1 - self.1) == -1 || (p.0 - self.0) == -1,
                     _ => false
                 }
-            ).collect();
+            )
+            .map(|(p, _)| p)
+            .collect();
 
         points
     }
 
-    fn start(&self, points: Vec<Point>) -> usize {
+    fn start(&self, points: HashMap<Point, char>) -> usize {
         let mut ways = self.ways(points.clone());
 
         let mut result = 1;
         let mut previous_points = vec![self.clone(), self.clone()];
-        while !ways.iter().all(|e| e.coords == ways[0].coords) {
+        while !ways.iter().all(|e| e == &ways[0]) {
             result += 1;
             let mut new_ways = Vec::new();
             for way in ways {
                 let prev = previous_points.remove(0);
-                let coords = way.next_point(prev).unwrap();
-                let point = points.iter().find(|p| p.coords == coords).unwrap();
-                new_ways.push(point.clone());
-                previous_points.push(way);
+                let char = points[&way];
+                let coords: Point = way.next_point(prev, char).unwrap();
+                new_ways.push(coords.clone());
+                previous_points.push(way.clone());
             }
             ways = new_ways;
         }
@@ -109,56 +110,39 @@ impl Point {
 fn main() {
     let time = std::time::Instant::now();
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let mut lines: Vec<Point> = Vec::new();
+    let mut points: HashMap<Point, char> = HashMap::new();
 
     for (i, line) in input.lines().enumerate() {
         let chars = line.chars();
         for (j, char) in chars.enumerate() {
-            lines.push(Point {
-                char,
-                coords: (j as i32, i as i32),
-            })
+            points.insert(Point(j as i32, i as i32), char);
         }
     }
 
-    let start_point = lines.iter().find(|p| p.char == 'S');
-    match start_point {
-        Some(s_point) => {
-            let point = s_point.clone();
-            let result = point.start(lines);
-            println!("Result {:?} ({:?})", result, time.elapsed())
-        }
-        _ => panic!("No start point")
-    }
+    let (start_point, _) = points.clone().into_iter().find(|(_, c)| c == &'S').unwrap();
+    let result = start_point.start(points);
+    println!("Result {:?} ({:?})", result, time.elapsed())
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use crate::Point;
 
     #[test]
     fn part_1() {
         let test = std::fs::read_to_string("test.txt").unwrap();
-        let mut lines: Vec<Point> = Vec::new();
+        let mut points: HashMap<Point, char> = HashMap::new();
 
         for (i, line) in test.lines().enumerate() {
             let chars = line.chars();
             for (j, char) in chars.enumerate() {
-                lines.push(Point {
-                    char,
-                    coords: (j as i32, i as i32),
-                })
+                points.insert(Point(j as i32, i as i32), char);
             }
         }
 
-        let start_point = lines.iter().find(|p| p.char == 'S');
-        match start_point {
-            Some(s_point) => {
-                let point = s_point.clone();
-                let result = point.start(lines);
-                assert_eq!(result, 8)
-            }
-            _ => panic!("No start point")
-        }
+        let (start_point, _) = points.clone().into_iter().find(|(_, c)| c == &'S').unwrap();
+        let result = start_point.start(points);
+        assert_eq!(result, 8)
     }
 }
